0e89320fd1b5bef30cbf649c4f36d902cd039ce9
Christo DeLange <cdelange@amplify.com>
2014-07-28 13:43:34 -0400

    0.9.10 slipped out so now we have to trump it

--- lib/cicd/builder/chefrepo/version.rb
+++ lib/cicd/builder/chefrepo/version.rb
@@ -6,7 +6,7 @@ module CiCd
       #   remove_const c if const_defined?(c)
       # end
 
-      VERSION  = '0.1.5'
+      VERSION  = '0.9.15'
       MAJOR, MINOR, TINY = VERSION.split('.')
       PATCH = TINY
     end
