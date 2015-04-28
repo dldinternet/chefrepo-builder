7b61185edc345a45a7dd397459a43c4d669066b7
Christo DeLange <github@dldinternet.com>
2015-04-28 16:03:14 -0400

    Create a classes.yaml file for deployments

--- lib/cicd/builder/chefrepo/version.rb
+++ lib/cicd/builder/chefrepo/version.rb
@@ -6,7 +6,7 @@ module CiCd
       #   remove_const c if const_defined?(c)
       # end
 
-      VERSION  = '0.9.31'
+      VERSION  = '0.9.32'
       MAJOR, MINOR, TINY = VERSION.split('.')
       PATCH = TINY
     end
