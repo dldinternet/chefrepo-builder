0aa55fafc97d2978a5320adc0520aab962bfb093
Christo DeLange <github@dldinternet.com>
2015-08-19 16:19:50 -0400

    Don't strip relative path of artifacts

--- lib/cicd/builder/chefrepo/version.rb
+++ lib/cicd/builder/chefrepo/version.rb
@@ -6,7 +6,7 @@ module CiCd
       #   remove_const c if const_defined?(c)
       # end
 
-      VERSION  = '0.9.43'
+      VERSION  = '0.9.44'
       MAJOR, MINOR, TINY = VERSION.split('.')
       PATCH = TINY
     end
